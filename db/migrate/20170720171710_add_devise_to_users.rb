a5712a697a315edb34234949d1f51e2fc92397ef
Jordan Hudgens <jordan@devcamp.com>
2017-07-22 17:40:24 +0000

    changed to make my gemfile work

--- db/migrate/20170720171710_add_devise_to_users.rb
+++ db/migrate/20170720171710_add_devise_to_users.rb
@@ -2,25 +2,25 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      #t.string :email,              null: false, default: ""
+      #t.string :encrypted_password, null: false, default: ""
 
       ## Custom Fields
-      t.string :Name
+      #t.string :Name
       
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      #t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.inet     :current_sign_in_ip
-      t.inet     :last_sign_in_ip
+      #t.integer  :sign_in_count, default: 0, null: false
+      #t.datetime :current_sign_in_at
+      #t.datetime :last_sign_in_at
+      #t.inet     :current_sign_in_ip
+      #t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -38,8 +38,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :user, :email,                unique: true
+    # add_index :user, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
