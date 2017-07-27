3d55c45bbe98a14034252a864ec35bcc0412b01e
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 16:50:40 +0000

    Updated styles for user edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,20 +3,20 @@ module ApplicationHelper
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
-      (link_to "Login",  new_user_session_path, class: style)    
+      (link_to "Login", new_user_session_path, class: style)
     else
-       link_to "Logout", destroy_user_session_path, method: :delete 
-    end 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
-  
+
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
-  
+
   def copyright_generator
-    ViewingViewTool::Renderer.copyright 'Alex Phillips', 'All rights reserved' 
+    ViewingViewTool::Renderer.copyright 'Alex Phillips', 'All rights reserved'
   end
 end
