d31047dda6ed0797ede31c007a8ba194b2dd0fcc
Jordan Hudgens <jordan@devcamp.com>
2017-08-07 18:58:46 +0000

    Implemented inital design for portfolio index page

--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20170720171710) do
     t.string   "slug"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "Name"
@@ -87,7 +88,6 @@ ActiveRecord::Schema.define(version: 20170720171710) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["slug"], name: "index_users_on_slug", unique: true, using: :btree
